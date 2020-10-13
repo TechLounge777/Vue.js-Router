9089182ecfe1b54b5c72eb9c9aae3daa9ada8bf8
TechLounge777 <msblanche777@gmail.com>
2020-10-13 22:24:36 +0200

    vue router

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from './views/Home'
+import About from './views/About'
+import Contact from './views/Contact'
+
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+    routes: [
+        { path: '', component: Home },
+        { path: '/about', component: About },
+        { path: '/contact', component: Contact }
+    ]
+})
